414d8136a0423f45a11ee607a9630afd684b5a23
jackiegatwiri <jaciegatz@gmail.com>
2019-03-31 10:59:09 +0300

    initialize project, generate token, create component myProfile

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyProfileComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
